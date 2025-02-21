fbff4ea9c5e4e434637733eb3085ee3bbe7c7e1b
pcyga <263983@student.pwr.edu.pl>
2025-02-21 20:48:53 +0100

    added compat

---
+++ src/main/java/net/paveuu/smolbartek/mixin/SmolBartekAccessor.java
@@ -0,0 +1,11 @@
+package net.paveuu.smolbartek.mixin;
+
+import net.minecraft.world.food.FoodProperties;
+import net.paveuu.smolbartek.item.ModFoods;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
+
+@Mixin(ModFoods.class)
+public interface SmolBartekAccessor {
+
+}
